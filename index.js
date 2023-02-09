7b20a825be96b03d685c9d394140a6deb3cc9dce
Khalil Merine <merinekh@gmail.com>
2023-02-09 16:53:07 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+var cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+// NEW CODE
+app.use(express.static("public"));
+// END OF NEW CODE
+
+// This displays message that the server running and listening to specified port
+
+app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
